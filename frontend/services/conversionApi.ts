0f0bc702d1d9abc1063b4d4177482c0f178a92dc
KRISH826 <krishnendupanja98@gmail.com>
2026-07-22 20:57:03 +0530

    Please provide the code changes or file diffs you would like me to summarize.

--- frontend/services/conversionApi.ts
+++ frontend/services/conversionApi.ts
@@ -1,8 +1,15 @@
 import { baseApi } from "./baseapi";
+import { Convertedfile } from "../types/upload";
+
+export interface ImageConversionResponse {
+    success: boolean;
+    message: string;
+    data: Convertedfile[];
+}
 
 export const imageApi = baseApi.injectEndpoints({
     endpoints: (builder) => ({
-        uploadandConvertImage: builder.mutation<any, FormData>({
+        uploadandConvertImage: builder.mutation<ImageConversionResponse, FormData>({
             query: (formData) => ({
                 url: "/image-converter",
                 method: "POST",
